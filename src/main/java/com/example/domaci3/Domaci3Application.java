76256f9a5645a31479d9b818378dbc8f48e915f9
Andrija <andrijadjelic@gmail.com>
2022-12-13 12:23:34 +0100

    Initial commit

---
+++ src/main/java/com/example/domaci3/Domaci3Application.java
@@ -0,0 +1,13 @@
+package com.example.domaci3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Domaci3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Domaci3Application.class, args);
+    }
+
+}
